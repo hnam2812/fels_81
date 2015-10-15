70a15cbbf06a73d5a0dbc5eaebb08dcf71d47665
hnam2812 <hnam2812@gmail.com>
2015-10-15 17:05:19 +0700

    Sign in/sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  
+  attr_accessor :remember_token
   has_many :activities
   has_many :lessons
   has_many :active_relationships, class_name: "Relationship",
@@ -22,11 +22,35 @@ class User < ActiveRecord::Base
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+      BCrypt::Engine.cost
     BCrypt::Password.create string, cost: cost
   end
 
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password? token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
   private
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest activation_token
+  end
+    
   def downcase_email
     self.email = email.downcase
   end
