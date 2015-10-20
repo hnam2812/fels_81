83e4ef2bbd44c803963fed1809ecf8e6d514adf0
duyth1993 <duyth1993@gmail.com>
2015-10-20 20:32:23 +0700

    User update profile and admin delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
   def index
     @users = User.paginate page: params[:page]
   end
@@ -18,12 +22,52 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by id: params[:id]
+    @user = User.find params[:id]
+  end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes user_params
+      flash[:success] = t "flash.user.update.success"
+      redirect_to @user
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    if User.find(params[:id]).destroy
+      flash[:success] = t "flash.user.delete.success"
+    else 
+      flash[:warning] = t "flash.user.delete.fail"
+    end
+    redirect_to users_url
   end
 
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t ".require_log_in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_url unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?      
   end
 end
