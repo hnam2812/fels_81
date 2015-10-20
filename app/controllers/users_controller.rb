c509f02afa1cfe6286dee74e36187b1774dbf4cb
duyth1993 <duyth1993@gmail.com>
2015-10-20 09:36:56 +0700

    Show all users and user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.paginate page: params[:page]
+  end
+
   def new
     @user = User.new
   end
@@ -11,6 +15,10 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Tang Hoai Duy", 
+  email: "duyth1993@gmail.com",
+  password: "123456",
+  password_confirmation: "123456",
+  admin: true)
+
+User.create!(name: "Nguyen Hoang Nam",
+  email: "hnam2812@gmail.com",
+  password: "123456",
+  password_confirmation: "123456",
+  admin: true)
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@fels.org"
+  password = "123456"
+  User.create!(name: name,
+    email: email,
+    password: password,
+    password_confirmation: password)
+end
